b87e3315d80c0b7d49dd1a659281a0bb4307d242
clarkGabber <gui.lepape25@gmail.com>
2021-12-09 00:39:30 +0100

    Add switch lol for level factory

--- src/Level/include/LevelFactory.h
+++ src/Level/include/LevelFactory.h
@@ -24,40 +24,41 @@ class LevelFactory {
  public:
   static std::unique_ptr<Level> newLevel(Player& player, const Options& options,
                                          const int& levelNumber) {
-    if (levelNumber == 0) {
-      return std::make_unique<Introduction>(player, options);
-    }
-    if (levelNumber == 1) {
-      return std::make_unique<Level1>(player, options);
-    }
-    if (levelNumber == 2) {
-      return std::make_unique<Level2>(player, options);
-    }
-    if (levelNumber == 3) {
-      return std::make_unique<Level3>(player, options);
-    }
-    if (levelNumber == 4) {
-      return std::make_unique<Level4>(player, options);
-    }
-    if (levelNumber == 5) {
-      return std::make_unique<Level5>(player, options);
-    }
-    if (levelNumber == 6) {
-      return std::make_unique<Level6>(player, options);
-    }
-    if (levelNumber == 7) {
-      return std::make_unique<Level7>(player, options);
-    }
-    if (levelNumber == 8) {
-      return std::make_unique<Level8>(player, options);
-    }
-    if (levelNumber == 9) {
-      return std::make_unique<Level9>(player, options);
-    }
-    if (levelNumber == 10) {
-      return std::make_unique<Level10>(player, options);
-    } else {
-      return std::make_unique<Outro>(player, options);
+    switch (levelNumber) {
+      case 0: {
+        return std::make_unique<Introduction>(player, options);
+      }
+      case 1: {
+        return std::make_unique<Level1>(player, options);
+      }
+      case 2: {
+        return std::make_unique<Level2>(player, options);
+      }
+      case 3: {
+        return std::make_unique<Level3>(player, options);
+      }
+      case 4: {
+        return std::make_unique<Level4>(player, options);
+      }
+      case 5: {
+        return std::make_unique<Level5>(player, options);
+      }
+      case 6: {
+        return std::make_unique<Level6>(player, options);
+      }
+      case 7: {
+        return std::make_unique<Level7>(player, options);
+      }
+      case 8: {
+        return std::make_unique<Level8>(player, options);
+      }
+      case 9: {
+        return std::make_unique<Level9>(player, options);
+      }
+      case 10: {
+        return std::make_unique<Level10>(player, options);
+      }
+      default: { return std::make_unique<Outro>(player, options); }
     }
   }
 };
