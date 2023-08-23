4dae29fbee20e625d4cf8f52d5466e7b4fdd6922
Guillaume Le Pape <gui.lepape25@gmail.com>
2023-08-23 13:06:22 +0200

    Make message writer more generic

---
+++ src/utils/include/tuple.h
@@ -0,0 +1,16 @@
+#ifndef TUPLE_H
+#define TUPLE_H
+
+namespace utils {
+template <class Tuple, class F>
+constexpr auto for_each(Tuple&& tuple, F&& f) {
+  return []<std::size_t... I>(Tuple&& tuple, F&& f, std::index_sequence<I...>) {
+    (f(std::get<I>(tuple)), ...);
+    return f;
+  }(std::forward<Tuple>(tuple), std::forward<F>(f),
+         std::make_index_sequence<
+             std::tuple_size<std::remove_reference_t<Tuple>>::value>{});
+}
+}  // namespace utils
+
+#endif
--- src/writer/src/message_writer.cpp
+++ src/writer/src/message_writer.cpp
@@ -1,43 +1,6 @@
 #include "message_writer.h"
 
-#include <fmt/color.h>
-
-#include "pause.h"
-
 namespace message {
-void write(const data::Message_t& messages, const std::string& pseudo,
-           const std::string& plugName) {
-  for (const auto& message : messages) {
-    utils::pause();
-
-    switch (std::get<0>(message)) {
-      case data::NameSpeaker::player:
-        fmt::print(fg(fmt::color::green) | fmt::emphasis::bold, "\n        {}",
-                   pseudo);
-        fmt::print(fg(fmt::color::blue) | fmt::emphasis::bold, "\n {}",
-                   std::get<1>(message));
-        break;
-
-      case data::NameSpeaker::plug:
-        fmt::print(fg(fmt::color::red) | fmt::emphasis::bold, "\n        {}",
-                   plugName);
-        fmt::print(fg(fmt::color::blue) | fmt::emphasis::bold, "\n {}",
-                   std::get<1>(message));
-        break;
-
-      case data::NameSpeaker::description:
-        fmt::print(fg(fmt::color::magenta) | fmt::emphasis::bold, "\n {}",
-                   std::get<1>(message));
-        break;
-
-      case data::NameSpeaker::action:
-        fmt::print(fg(fmt::color::yellow) | fmt::emphasis::bold, "\n {}",
-                   std::get<1>(message));
-        break;
-    }
-  }
-}
-
 void write(const MessageWriter& messageWriter) {
   write(messageWriter.messages_, messageWriter.pseudo_,
         messageWriter.plugName_);
