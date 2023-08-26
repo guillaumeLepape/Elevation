d49c9e01ee4d2970ad3a8d49ef145cf3c4e2b082
Guillaume Le Pape <gui.lepape25@gmail.com>
2023-08-26 19:02:12 +0200

    Removing a useful function

--- src/data/include/list_name_data.h
+++ src/data/include/list_name_data.h
@@ -1,12 +1,14 @@
 #ifndef NAME_DATA_H
 #define NAME_DATA_H
 
+#include <filesystem>
 #include <set>
 #include <string>
 
+namespace fs = std::filesystem;
+
 namespace data {
-std::set<std::string> read_list_name(std::string_view&& nameFolder,
-                                     std::string_view&& nameFile);
+std::set<std::string> read_list_name(fs::path&& file_path);
 }
 
 #endif
